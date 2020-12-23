4e27ca44acf8db324127dda2a618538a61cc9eb2
Shane Slone <slone.shane@gmail.com>
2020-12-23 02:36:48 -0500

    deleted an unused import

--- src/main/java/com/lambdaschool/africanmarketplace/SeedData.java
+++ src/main/java/com/lambdaschool/africanmarketplace/SeedData.java
@@ -11,7 +11,6 @@ import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Locale;
 
